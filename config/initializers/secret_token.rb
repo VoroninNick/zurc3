efae29b58121124129fc2d466001c9a7c99cb4ff
pasha <p.korenev@voroninstudio.eu>
2013-12-02 05:42:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zurc::Application.config.secret_key_base = '0f3fe468cddd3ba48dbdfa2e8f104d1a4e0d3d62f380d99cb6e8b51e9f78472b3af4d6386fd6599033b6f4819a89761c38227b865d92ee5a84075c45c0dcba27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zurc::Application.config.session_store :cookie_store, key: '_zurc_session'
