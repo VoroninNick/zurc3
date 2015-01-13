3e924485f9391e128c8e212470daad88dd53b377
VoroninNick <p.korenev@voroninstudio.eu>
2015-01-13 12:10:05 +0200

    add routes in admin

--- app/admin/route.rb
+++ app/admin/route.rb
@@ -1,28 +1,26 @@
-if ActiveRecord::Base.connection.tables.include?(:routes)
-  ActiveAdmin.register Vs::Route do
-    menu parent: proc { I18n.t("menu.labels.seo") }
+ActiveAdmin.register Vs::Route do
+  menu parent: proc { I18n.t("menu.labels.seo") }
 
-    sortable
+  sortable
 
-    # See permitted parameters documentation:
-    # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-    #
-    # permit_params :list, :of, :attributes, :on, :model
-    #
-    # or
-    #
-    # permit_params do
-    #   permitted = [:permitted, :attributes]
-    #   permitted << :other if resource.something?
-    #   permitted
-    # end
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
 
-    index :as => :sortable do
-      label :route_name
+  index :as => :sortable do
+    label :route_name
 
-      actions
-    end
-
-
+    actions
   end
+
+
 end
